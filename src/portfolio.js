86b19dc023496200bd68cd850dd2132478cf28ec
root <jahmed.cloud@outlook.com>
2023-09-30 21:20:22 +0000

    updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const settings = {
     subTitle:
       "Results-driven IT professional with a strong focus on cloud computing, infrastructure management, and system architecture. Proficient in PowerShell scripting and Terraform, I excel in crafting efficient and automated solutions.",
     resumeLink:
-      "https://drive.google.com/",
+      "https://drive.google.com/drive/folders/1bZ7du3Tk--2u8cRYlXGBF5iyPX07nK0K?usp=sharing",
     portfolio_repository: "https://github.com/jahmed-cloud",
     githubProfile: "https://github.com/jahmed-cloud",
   };
@@ -72,9 +72,9 @@ const settings = {
               "⚡I've showcases a diverse skill set in cloud infrastructure architecture, with a focus on Microsoft Azure services.",
               "⚡I've successfully deployed and managed Azure Active Directory, Azure Virtual Desktop, Windows 365, and Microsoft Endpoint Manager to empower organizations with secure, efficient, and scalable solutions",
               "⚡My expertise in implementing Microsoft 365 BCDR strategies, integration, automation, and proactive monitoring reflects a commitment to delivering resilient and high-performing cloud environments for clients.",
-              "⚡I spearheaded the implementation of Azure AD, fortifying identity and access management with seamless single sign-on (SSO) capabilities. Enhanced security was ensured through the enforcement of multi-factor authentication (MFA). Additionally, I orchestrated a seamless integration of Azure AD with on-premises Active Directory to support hybrid environments.",
               "⚡I meticulously designed and deployed Azure Virtual Desktop solutions, enabling efficient remote work. I optimized virtual machine configurations for performance and cost-efficiency, while also implementing auto-scaling mechanisms to adapt to fluctuating user demands. Security was a priority, addressed through Azure Disk Encryption and Network Security Groups.",
               "⚡My expertise enabled seamless integration between Azure services and Microsoft 365, automating routine tasks through Azure Logic Apps and Power Automate. I orchestrated workflows for resource provisioning and deprovisioning, ultimately improving operational efficiency and reducing manual intervention.",     
+              "⚡Managed and optimized Azure Firewall, implementing automation for updates and auto-scaling to bolster security and performance while reducing operational overhead.",            
             ],
             softwareSkills: [
                {
@@ -132,11 +132,11 @@ const settings = {
             title: "Reporting and Alerting",
             fileName: "DataScienceImg",
             skills: [
-              "⚡ I've leveraged Microsoft Power BI and Power Automate to transform data into actionable insights and automate business processes.",
-              "⚡ I've built an Azure Workbook utilizing Kusto Query Language (KQL) to effectively manage over 1000 virtual machines, ensuring their timely updates and compliance.",
-              "⚡ Created alerts based on collected data, enhancing operational efficiency, and proactively addressing potential issues.",
-              "⚡ My holistic approach to data analytics and infrastructure management contributes to robust, data-driven decision-making and operational excellence.",
-              "⚡ To proactively monitor and secure the environment, I implemented Azure Monitor and Azure Security Center. This encompassed setting up custom alerts and dashboards to track resource performance and security. Additionally, I generated insightful reports on usage, compliance, and security incidents, ensuring ongoing enhancements through data-driven insights.",
+              "⚡Crafted impactful Power BI reports by harnessing data from Log Analytics Workspace, enabling data-driven insights and automated data refreshing at 30-minute intervals, empowering teams to make data-backed decisions.",
+              "⚡I've built an Azure Workbook utilizing Kusto Query Language (KQL) to effectively manage over 1000+ virtual machines, ensuring their timely updates and compliance.",
+              "⚡Created alerts based on collected data, enhancing operational efficiency, and proactively addressing potential issues.",
+              "⚡My holistic approach to data analytics and infrastructure management contributes to robust, data-driven decision-making and operational excellence.",
+              "⚡To proactively monitor and secure the environment, I implemented Azure Monitor and Azure Security Center. This encompassed setting up custom alerts and dashboards to track resource performance and security. Additionally, I generated insightful reports on usage, compliance, and security incidents, ensuring ongoing enhancements through data-driven insights.",
             ],
             softwareSkills: [
               {
@@ -230,9 +230,9 @@ const settings = {
         alt_name: "IIITDM Kurnool",
         duration: "2016 - 2020",
         descriptions: [
-          "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-          "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-          "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+          "âš¡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+          "âš¡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+          "âš¡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
         ],
         website_link: "http://iiitk.ac.in",
       },
@@ -243,9 +243,9 @@ const settings = {
         alt_name: "Indiana University Bloomington",
         duration: "2021 - 2023",
         descriptions: [
-          "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
-          "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
-          "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+          "âš¡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
+          "âš¡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
+          "âš¡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
         ],
         website_link: "https://www.indiana.edu/",
       },
@@ -385,7 +385,7 @@ const settings = {
                 duration: "June 2021 to Aug 2023",
                 location: "Hyderabad, Telangana",
                 description:
-                  "As a Technical Lead, I have been actively engaged in end-to-end agile processes for Azure Virtual Desktop, Windows 365, PowerBI, and related services. I've played a key role in effort estimation, cost analysis, and requirements analysis, ensuring seamless project flows. My responsibilities also include coordinating with both onshore and offshore teams, providing guidance, optimizing infrastructure performance, and managing migrations to Microsoft Azure’s WVD and W365. Additionally, I've led a team in creating impactful PowerBI dashboards and Azure workbooks using KQL, contributing to robust data-driven decision-making.",
+                  "As a Technical Lead, I have been actively engaged in end-to-end agile processes for Azure Virtual Desktop, Windows 365, PowerBI, and related services. I've played a key role in effort estimation, cost analysis, and requirements analysis, ensuring seamless project flows. My responsibilities also include coordinating with both onshore and offshore teams, providing guidance, optimizing infrastructure performance, and managing migrations to Microsoft Azureâ€™s WVD and W365. Additionally, I've led a team in creating impactful PowerBI dashboards and Azure workbooks using KQL, contributing to robust data-driven decision-making.",
                 color: "#0879bf",
               },
               {
@@ -579,7 +579,7 @@ const settings = {
   const contactPageData = {
     contactSection: {
       title: "Contact Me",
-      profile_image_path: "animated_ashutosh.png",
+      profile_image_path: "animated_junaid.png",
       description:
         "Connect with me online to discuss cloud solutions, infrastructure, PowerBI, and Azure Virtual Desktop strategies. Let's explore how I can contribute to your success.",
     },
@@ -593,12 +593,12 @@ const settings = {
     addressSection: {
       title: "Address",
       subtitle:
-        "09 1st floor, L R Bande road, Shampura, Bangalore 560032",
+        "",
       locality: "Bangalore",
       country: "IN",
       region: "Karnataka",
-      postalCode: "560032",
-      streetAddress: "L R Bande road",
+      postalCode: "",
+      streetAddress: "",
       avatar_image_path: "address_image.svg",
       location_map_link: "",
     },
